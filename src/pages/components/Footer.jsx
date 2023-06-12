41a8ee593e9251c5ce9b15d8b91680f93fbb6306
Aubriot Marc <marc.aubriot@outlook.fr>
2023-06-12 20:34:51 +0200

    responsive footer

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -5,49 +5,52 @@ const Footer = () => {
     return (
         <footer>
             
-            <div className='footerTopBox'>
-                <div className='addressePanel'>
-                    <p>Où nous trouver?</p>
-                    <p>6, rue de Bruxelles</p>
-                    <p>57370 PHALSBOURG</p>
-                    <p>Zone industrielle</p>
-                    <p>D46, 57370SCHALBACH</p>
-                </div>
-
-                <div className='contactPanel'>
-                    <p>Nous contacter</p>
-                    <p>Téléphone</p>
-                    <p>Email</p>
-                    <p>Contact en ligne</p>
-                </div>
-            </div>
-
-            <div className='footerBotBox'>
-                <p>Horaires d'ouvertures</p>
-                
-                <div className='ouvertureBox'>
-                    <div className='daynameBox'>
-                        <p>Lundi</p>
-                        <p>Mardi</p>
-                        <p>Mercredi</p>
-                        <p>Jeudi</p>
-                        <p>Vendredi</p>
-                        <p>Samedi</p>
-                        <p>Dimanche</p>
+            <div className='footerContainer'>
+            
+                <div className='footerTopBox'>
+                    <div className='addressePanel'>
+                        <p>Où nous trouver?</p>
+                        <p>6, rue de Bruxelles</p>
+                        <p>57370 PHALSBOURG</p>
+                        <p>Zone industrielle</p>
+                        <p>D46, 57370SCHALBACH</p>
                     </div>
+
+                    <div className='contactPanel'>
+                        <p>Nous contacter</p>
+                        <p>Téléphone</p>
+                        <p>Email</p>
+                        <p>Contact en ligne</p>
+                    </div>
+                </div>
+
+                <div className='footerBotBox'>
+                    <p>Horaires d'ouvertures</p>
                     
-                    <div className='horairesBox'>
-                        <p>08h00 - 12h00 / 13h00 - 18h00</p>
-                        <p>08h00 - 12h00 / 13h00 - 18h00</p>
-                        <p>08h00 - 12h00 / 13h00 - 18h00</p>
-                        <p>08h00 - 12h00 / 13h00 - 18h00</p>
-                        <p>08h00 - 12h00 / 13h00 - 18h00</p>
-                        <p>08h00 - 12h00 / 13h00 - 18h00</p>
-                        <p>fermé</p>
+                    <div className='ouvertureBox'>
+                        <div className='daynameBox'>
+                            <p>Lundi</p>
+                            <p>Mardi</p>
+                            <p>Mercredi</p>
+                            <p>Jeudi</p>
+                            <p>Vendredi</p>
+                            <p>Samedi</p>
+                            <p>Dimanche</p>
+                        </div>
+                        
+                        <div className='horairesBox'>
+                            <p>08h00 - 12h00 / 13h00 - 18h00</p>
+                            <p>08h00 - 12h00 / 13h00 - 18h00</p>
+                            <p>08h00 - 12h00 / 13h00 - 18h00</p>
+                            <p>08h00 - 12h00 / 13h00 - 18h00</p>
+                            <p>08h00 - 12h00 / 13h00 - 18h00</p>
+                            <p>08h00 - 12h00 / 13h00 - 18h00</p>
+                            <p>fermé</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
             <p>Copyright 2023 - Mentions légales - Charte de confidentialité</p>
         </footer>
     )
