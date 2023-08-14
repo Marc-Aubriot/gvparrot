eba9742fd80ea5def34a3dbb0a23c94f567c3e35
Aubriot Marc <marc.aubriot@outlook.fr>
2023-08-14 14:18:04 +0200

    admin can access every fx from backoffice

--- src/pages/components/BackofficeNavbar.jsx
+++ src/pages/components/BackofficeNavbar.jsx
@@ -40,10 +40,15 @@ const BackofficeNavbar = (props) => {
             
                 <ul className={`backofficemenuNav ${navbarOpen ? " showMenu" : ""}`}>
                     <li><Link to={`${props.id}/accueil`} onClick={closeMenu}>Accueil espace de travail</Link></li>
+                    <li><Link to={`${props.id}/addcar`} onClick={closeMenu}>Ajouter une voiture au catalogue</Link></li>
                     <li><Link to={`${props.id}/addemployee`} onClick={closeMenu}>Ajouter un employé</Link></li>
                     <li><Link to={`${props.id}/modifyhoraires`} onClick={closeMenu}>Définir les horaires d'ouverture</Link></li>
+                    <li><Link to={`${props.id}/equipmentlist`} onClick={closeMenu}>Gérer la liste des équipements</Link></li>
+                    <li><Link to={`${props.id}/carlist`} onClick={closeMenu}>Gérer le catalogue de voitures</Link></li>
+                    <li><Link to={`${props.id}/verifycomments`} onClick={closeMenu}>Modération de commentaires</Link></li>
                     <li><Link to={`${props.id}/modifyservices`} onClick={closeMenu}>Modifier les services</Link></li>
                     <li><Link to={`${props.id}/listemployee`} onClick={closeMenu}>Voir la liste des employés</Link></li>
+                    <li><Link to={`${props.id}/mailbox`} onClick={closeMenu}>Voir tous les messages reçus</Link></li>
                     <li className="deconnexionBTN"><Link to={`${props.id}/logout`} onClick={closeMenu}>Déconnexion</Link></li>
                 </ul>
 
